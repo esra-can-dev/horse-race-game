f20260b8f9e19d6bc74d0bc17927ff88385b5008
Esra Can <esra.can.dev@gmail.com>
2025-06-10 12:55:35 +0300

    initial commit

---
+++ src/views/GameView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-3xl font-bold text-green-600">
+    Horse Racing Game 🐎
+  </div>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    environment: 'jsdom',
+  },
+  server: {
+    port: 3030
+  }
+})
